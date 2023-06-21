63fb328fa611933f1636add0cc2c2747b8a91235
Zappp3 <119132633+Zappp3@users.noreply.github.com>
2023-06-21 19:09:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,92 @@
+const api = {
+  key: "2fa73590fd8b5a4c6e68098ad5625395",
+  base: "https://api.openweathermap.org/data/2.5/"
+};
+
+const searchbox = document.querySelector(".search-box");
+searchbox.addEventListener("keypress", setQuery);
+
+function setQuery(evt) {
+  if (evt.keyCode == 13) {
+    getResults(searchbox.value);
+  }
+}
+
+function getResults(query) {
+  fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+    .then((weather) => {
+      return weather.json();
+    })
+    .then(displayResults);
+}
+function displayResults(weather) {
+  console.log(weather);
+  let city = document.querySelector(".location .city");
+  city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+  let now = new Date();
+  let date = document.querySelector(".location .date");
+  date.innerText = dateBuilder(now);
+
+  let temp = document.querySelector(".current .temp");
+  temp.innerHTML = `${Math.round(weather.main.temp)}<span>°C</span>`;
+
+  let weather_el = document.querySelector(".current .weather");
+  weather_el.innerText = weather.weather[0].main;
+
+  let hilow = document.querySelector(".hi-low");
+  hilow.innerText = `${weather.main.temp_min}°C / ${weather.main.temp_max}°C`;
+
+  if(weather.weather[0].main=="Rain"){
+    document.body.style.backgroundImage="url('images/rain.gif')";
+  }
+  else if(weather.weather[0].main=="Clear"){
+    document.body.style.backgroundImage="url('images/clear.gif')";
+  }
+  else if(weather.weather[0].main=="Lightning"){
+    document.body.style.backgroundImage="url('images/lightning.gif')";
+  }
+  else if(weather.weather[0].main=="Sunny"){
+    document.body.style.backgroundImage="url('images/sunny.gif')";
+  }
+  else if(weather.weather[0].main=="Clouds"){
+    document.body.style.backgroundImage="url('images/cloudy.gif')";
+  }
+  else{
+    document.body.style.backgroundImage="url('https://images.pexels.com/photos/1118873/pexels-photo-1118873.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')";
+  }
+
+}
+
+function dateBuilder(d) {
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+
+  return `${day} ${date} ${month} ${year}`;
+}
